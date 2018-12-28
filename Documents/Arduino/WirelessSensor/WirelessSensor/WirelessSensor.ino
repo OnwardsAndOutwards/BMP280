c974051fa4e01f1e9f3c74e18a44b795623bfa94
Daniel Thomas <32112975+OnwardsAndOutwards@users.noreply.github.com>
2018-12-27 21:51:37 -0800

    It seemed broken, now it seems fixed.

--- Documents/Arduino/WirelessSensor/WirelessSensor/WirelessSensor.ino
+++ Documents/Arduino/WirelessSensor/WirelessSensor/WirelessSensor.ino
@@ -1,65 +1,69 @@
-#define BLYNK_PRINT Serial  // For communicating with Blynk android app via the cloud
-#include <SPI.h>  // Arduino Serial Peripheral Interface
-#include <DHT.h>  // DHT Temperature / Humidity Sensor
-#include <BlynkSimpleEsp8266.h>  // Onboard ESP control for da internetz
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
 
+#define BLYNK_PRINT Serial
+#include <BlynkSimpleEsp8266.h>
 
 char auth[] = "2fa8e0076d4e4d79add6a9ab2f7b18ae"; // Blynk Token
 char ssid[] = "CowboyBeepBoop"; // Wireless Network
 char pass[] = "GARYoldman!"; // Wifi Password
 
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
 
-#define DHTPIN 4
-
-//#define DHTTYPE DHT11     // DHT 11
-//#define DHTTYPE DHT21   // DHT 21, AM2301
-#define DHTTYPE DHT22   // DHT 22, AM2302, AM2321
-
-DHT dht(DHTPIN, DHTTYPE);
 BlynkTimer timer;
 
-// This function sends Arduino's up time every second to Virtual Pin (5).
-// In the app, Widget's reading frequency should be set to PUSH. This means
-// that you define how often to send data to Blynk App.
-void sendSensor()
-{
-  float h = dht.readHumidity();
-  float t = dht.readTemperature(); // or dht.readTemperature() if you ain't using `murican
-  float tf = dht.readTemperature(true);
+Adafruit_BMP280 bme; // I2C
+//Adafruit_BMP280 bme(BMP_CS); // hardware SPI
+//Adafruit_BMP280 bme(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V5, h);
-  Blynk.virtualWrite(V6, t);
-  Blynk.virtualWrite(V7, tf);
-  Blynk.virtualWrite(V0, 16);
+void sendSensor() {
+
 }
 
-void setup()
-{
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("BMP280 test"));
 
-  Serial.begin(9600);  // Debug console via serial monitor
-
-  //  Blynk.begin(auth);
   Blynk.begin(auth, ssid, pass);
 
-  dht.begin();
-
-  // Setup a function to be called every second
-  timer.setInterval(1000L, sendSensor);
+  if (!bme.begin()) {
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (1);
+  }
 }
 
-void loop()
-{
+void loop() {
+
   Blynk.run();
   timer.run();
 
-  float h = dht.readHumidity();
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" *C / ");
+  Serial.print((9 * bme.readTemperature() / 5) + 32);
+  Serial.println(" *F / ");
 
-  delay(1000);
+  Serial.print("Pressure = ");
+  Serial.print(bme.readPressure());
+  Serial.println(" Pa");
 
+  Serial.print("Approx altitude = ");
+  Serial.print(bme.readAltitude(1030.00)); // this should be adjusted to your local forcase
+  Serial.println(" m / ");
+  Serial.print((bme.readAltitude(1030.00) * 3.28 )); // this should be adjusted to your local forcase
+  Serial.println(" ft");
+
+  Serial.println();
+  delay(2000);
+
+  Blynk.virtualWrite(V4, int(bme.readAltitude(1030.00)));
+  Blynk.virtualWrite(V5, int((bme.readAltitude(1030.00) * 3.28 )));
+  Blynk.virtualWrite(V6, int(bme.readTemperature()));
+  Blynk.virtualWrite(V7, int((9 * bme.readTemperature() / 5) + 32));
+  //  Blynk.virtualWrite(V0, 16);
 }
